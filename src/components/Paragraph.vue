9e1bcd9bbede53e1e5824736e386c4363a0ce77f
Andrii <shostatskyi.andrii@gmail.com>
2023-01-06 22:48:01 +0100

    Init

---
+++ src/components/Paragraph.vue
@@ -0,0 +1,20 @@
+<template>
+  <node-view-wrapper style="margin: 10px;">
+    <div style="display: flex">
+      <div></div>
+      <node-view-content as="p" />
+    </div>
+  </node-view-wrapper>
+</template>
+
+<script>
+import { NodeViewWrapper, NodeViewContent, nodeViewProps } from '@tiptap/vue-3'
+
+export default {
+  components: {
+    NodeViewWrapper,
+    NodeViewContent,
+  },
+  props: nodeViewProps,
+}
+</script>
